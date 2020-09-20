701b18896a6e9fd7243a7eb09f9340567605f2af
PC-02 <pranavc10@hotmail.com>
2020-09-20 20:11:23 +0400

    Added Attendance Script w CSV file

---
+++ data/scripts/talalAttendance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while [ true ]
+do
+	day=$(date +'%d/%m/%Y')
+
+	time=$(cat /proc/uptime | awk '{ print $1 }')
+	time=${time%.*}
+	time=$(( $time/60/60 ))
+
+	if [ $time -gt 2 ]
+	then
+		echo "${day},1" >> Attendance.csv
+	else
+		echo "${day},0.5" >> Attendance.csv
+	fi
+
+	sleep 900
+done
